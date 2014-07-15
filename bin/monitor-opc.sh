6cb6a497e80bbf7da91e745f72b9b7f7ec40e82e
mpinner <mpinner@gmail.com>
2014-07-15 13:41:39 -0600

    fix path

--- bin/monitor-opc.sh
+++ bin/monitor-opc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until /home/debian/opc-server.sh; do
+until /home/debian/git/Active/bin/opc-server.sh; do
     echo "OPC Server crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
