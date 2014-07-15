c58f34577c12e34d391322ea64a960b6212e8881
mpinner <mpinner@gmail.com>
2014-07-15 17:46:38 -0600

    adding to the fun

--- bin/active-slinger.sh
+++ bin/active-slinger.sh
@@ -2,12 +2,12 @@
 
 LOG=/var/log/active-streamers.log 
 
-export OPC_SERVER= '192.168.2.7:7890'
+export OPC_SERVER=192.168.163.141:7890
 
 
-ls -ctr /home/debian/git/Active/streamers/*.py | while read script
+ls -ctr ./streamers/*.py | while read script
 do
   echo "running '$script'..." >> $LOG  2>&1
-  $script  >> $LOG  2>&1
+  timeout 10s $script  >> $LOG  2>&1
   echo "done '$script'"  >> $LOG  2>&1
 done
--- bin/monitor-slinger.sh
+++ bin/monitor-slinger.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-until /home/debian/git/Active/bin/active-slinger.sh; do
-    echo "Active Slinger crashed with exit code $?.  Respawning.." >&2
-    sleep 1
+cd /home/debian/git/Active;
+
+while true; do
+
+    until ./bin/active-slinger.sh; do
+        echo "Active Slinger crashed with exit code $?.  Respawning.." >&2
+        sleep 1
+    done
 done
