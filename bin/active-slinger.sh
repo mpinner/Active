13816a10c5ca5b945a83cf2d1f7ce89cab5e1c38
mpinner <mpinner@gmail.com>
2014-07-16 12:20:16 -0600

    scans MMMC

--- bin/active-slinger.sh
+++ bin/active-slinger.sh
@@ -5,7 +5,7 @@ LOG=/var/log/active-streamers.log
 export OPC_SERVER=192.168.163.141:7890
 
 
-ls -ctr ./streamers/*.py | while read script
+ls -ctr ./streamers/*.py ./streamers/active-streams/*.py | while read script
 do
   echo "running '$script'..." >> $LOG  2>&1
   timeout 10s $script  >> $LOG  2>&1
