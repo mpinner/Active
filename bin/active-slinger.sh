6e6cca994178d44ecbe171b19bdbe8aeb0e3511a
mpinner <mpinner@gmail.com>
2014-08-09 17:18:40 -0600

    updating for latest patterns

--- bin/active-slinger.sh
+++ bin/active-slinger.sh
@@ -5,8 +5,8 @@
 . /boot/uboot/env.sh
 
 cd $ACTIVE_HOME/src/
-echo "running against ./black-rings -v -layout $LAYOUT -server $OPC_SERVER" >> $STREAMER_LOG  2>&1
-./mixer -layout $LAYOUT -server $OPC_SERVER >> $STREAMER_LOG  2>&1
+echo "running against ./images -layout $LAYOUT -server $OPC_SERVER" >> $STREAMER_LOG  2>&1
+./images -layout $LAYOUT -server $OPC_SERVER >> $STREAMER_LOG  2>&1
 
 ## loop through streamers
 #ls -ctr ./streamers/*.py ./streamers/active-streams/*.py | while read script
