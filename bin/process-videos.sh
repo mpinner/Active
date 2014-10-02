589fd17754534f2e42dffb1a56ce3ab34d29f286
mpinner <mpinner@gmail.com>
2014-10-01 17:28:42 -0700

    changed directory structure for video pre-processing

--- bin/process-videos.sh
+++ bin/process-videos.sh
@@ -5,13 +5,16 @@
 # 
 # to be run repeatedly for making videos into small images
 
+. /boot/uboot/env.sh
+
 
 VIDEO_DIR=$THUMB_DRIVE/video
+INCOMING_DIR=$VIDEO_DIR/incoming
 LOCK_FILE=$VIDEO_DIR/lock/
 VIDEO_EXT=mp4
-VIDEO_FILE_PATTERN=$VIDEO_DIR/*.$VIDEO_EXT
-QUEUE_DIR=$VIDEO_DIR/queue/
-DONE_DIR=$VIDEO_DIR/done/
+VIDEO_FILE_PATTERN=$INCOMING_DIR/*.$VIDEO_EXT
+QUEUE_DIR=$VIDEO_DIR/playlist/
+DONE_DIR=$VIDEO_DIR/backup/
 
 
 #test for thumbdrive dir
@@ -25,11 +28,19 @@ if [ ! -d "$VIDEO_DIR" ]; then
     exit 1
 fi
 
+if [ ! -d "$INCOMING_DIR" ]; then
+  mkdir -p $INCOMING_DIR
+  if [ ! -d "$INCOMING_DIR" ]; then
+      echo "Video Incoming Directory not found!"
+      exit 1
+    fi
+fi
+
 if [ ! -d "$QUEUE_DIR" ]; then
-	mkdir -p $QUEUE_DIR
-	if [ ! -d "$QUEUE_DIR" ]; then
-   		echo "Video Queue Directory not found!"
-    	exit 1
+  mkdir -p $QUEUE_DIR
+  if [ ! -d "$QUEUE_DIR" ]; then
+      echo "Video Queue Directory not found!"
+      exit 1
     fi
 fi
 
@@ -50,6 +61,9 @@ if [[ -z $FILES ]]; then
     exit 1
 fi
 
+
+# New videos !!
+
 # set lock
 if mkdir $LOCK_FILE; then
   echo "Locking succeeded" >&2
@@ -67,8 +81,8 @@ for video in $FILES; do
   echo $name;
 
   #process video
-  echo "ffmpeg -i $video -vf scale=60:24 -r 15 -f image2 $DONE_DIR$name%03d.png"
-  time ffmpeg -i $video -vf scale=60:24 -r 15 -f image2 $QUEUE_DIR$name%06d.png
+  echo "/usr/local/bin/ffmpeg -i $video -vf scale=60:24 -r 15 -f image2 $DONE_DIR$name%03d.png"
+  time /usr/local/bin/ffmpeg -i $video -vf scale=60:24 -r 15 -f image2 $QUEUE_DIR$name%06d.png
 
   #move to done
   mv $video $DONE_DIR
@@ -79,4 +93,5 @@ done
 #remove lock
 rm -r $LOCK_FILE
 
+sudo killall -r video
 
---
+++ bin/rsync-backups.sh
@@ -0,0 +1,1 @@
+rsync -aEv --delete-during --exclude-from=../bbb-rsync-excludes.txt root@192.168.163.115:/ .
